e7a032817b7498782cbee2e5be55783b59e6180d
Hugh <hugh@lidexuMacBook-Pro.local>
2019-10-30 17:16:28 +0800

    change uploadlst

--- src/lstkit/gen_uploadlst.py
+++ src/lstkit/gen_uploadlst.py
@@ -1,5 +1,5 @@
 """
-Generate upload list from label list.
+Generate upload list from source file.
 format: key, source file path
 """
 import os
@@ -10,8 +10,8 @@ parser = argparse.ArgumentParser(description='generate upload list')
 parser.add_argument('--perfix', dest='perfix',
                     help='the perfix of image',
                     default='', type=str)
-parser.add_argument('--lst', dest='source_lst',
-                    help='source image list',
+parser.add_argument('--spath', dest='source_path',
+                    help='source file path',
                     default=None, type=str, required=True)
 
 args = parser.parse_args()
@@ -20,22 +20,16 @@ localtime = time.localtime(time.time())
 date = date = str(localtime.tm_mon) + str(localtime.tm_mday)
 
 perfix = args.perfix
-labellst = args.lst
+path = args.source_path
 uplist = "up"+date+".lst"
 
-up = []
-with open(labellst, 'r') as fu:
-	while True:
-		line = fu.readline()
-		if not line:
-			break
-		label = [i.strip() for i in line.strip().split()]
-		srcpath = label[0]
-		path = [j.strip() for j in srcpath.strip().split('/')]
-		key = "infant/" + path[-1]
+file_list = os.listdir(path)
 
-		newline = '%s,%s' % (key, srcpath)
-		up.append(newline)
+for i in file_list:
+	srcpath = path + '/' + i
+	key = perfix + '/' + i
+	newline = '%s,%s' % (key, srcpath)
+	up.append(newline)
 
 with open(uplist, 'w') as fu:
 	for l in up:
